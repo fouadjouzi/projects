c7b3ed3b452b001df8e44fd1d43bb89f89e00252
fouadjouzi <fouad.djouzi@outlook.com>
2024-11-26 21:16:48 +0100

    Add files via upload

---
+++ todoapp/src/App.jsx
@@ -0,0 +1,13 @@
+import Todo from './components/todo';
+import Header from './components/Header';
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
